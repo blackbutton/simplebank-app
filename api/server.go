f250e487b2bbeed642abff99ba97e1b84af9fab8
hhl_3131@163.com <haygon2020@outlook.com>
2022-03-19 22:29:56 +0800

    创建自定义Gin的字段验证函数

--- api/server.go
+++ api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	db "simplebank-app/db/sqlc"
 )
 
@@ -14,12 +16,15 @@ type Server struct {
 func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
-
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		var _ = v.RegisterValidation("currency", validCurrency)
+	}
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
 	router.PUT("/accounts/:id", server.updateAccount)
 	router.DELETE("/accounts/:id", server.deleteAccount)
+	router.POST("/transfers", server.createTransfer)
 	server.router = router
 	return server
 }
